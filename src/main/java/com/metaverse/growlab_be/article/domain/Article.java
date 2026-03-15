bb08d2846dcff92a813b129f2813439c81dfb977
hangabin1 <hgb101931@g.hongik.ac.kr>
2026-03-15 18:12:29 +0900

    feat: Article & Comment 엔티티 생성

---
+++ src/main/java/com/metaverse/growlab_be/article/domain/Article.java
@@ -0,0 +1,23 @@
+package com.metaverse.growlab_be.article.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "article")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+}
---
+++ src/main/java/com/metaverse/growlab_be/comment/domain/Comment.java
@@ -0,0 +1,20 @@
+package com.metaverse.growlab_be.comment.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+}
