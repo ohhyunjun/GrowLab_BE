4b65bf07f10e3ede8fa60c0cada8fa8c0f712939
JDoyoung <airwalk6346@gmail.com>
2026-03-15 14:27:30 +0900

    feat: 로그인 기능 구현(컨트롤러 기반 인증 패턴)

--- src/main/java/com/metaverse/growlab_be/auth/controller/AuthController.java
+++ src/main/java/com/metaverse/growlab_be/auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.metaverse.growlab_be.auth.controller;
 
+import com.metaverse.growlab_be.auth.dto.AuthResponseDto;
+import com.metaverse.growlab_be.auth.dto.LoginRequestDto;
 import com.metaverse.growlab_be.auth.dto.SignUpRequestDto;
 import com.metaverse.growlab_be.auth.service.UserService;
 import com.metaverse.growlab_be.auth.util.JwtUtil;
@@ -8,6 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +36,32 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthResponseDto> authenticateUser(@Valid @RequestBody LoginRequestDto loginRequestDto) {
+        try {
+            // 인증 객체 생성
+            // Authentication Manager를 통해서 username, password 기반으로 인증 수행 지시
+            // loadUserByUsername과 passwordEncoder.matches()가 내부적으로 실행
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequestDto.getUsername(),
+                            loginRequestDto.getPassword()
+                    )
+            );
+            // 결과물 인증 객체 (UserDetails == Principal)
+            // @AuthenticationPrincipal 애너테이션이 내부적으로 (UserDetails) 형변환을 해줌
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            // JWT 토큰 생성 (accessToken = jwtToken)
+            String accessToken = jwtUtil.generateToken(userDetails.getUsername());
+
+            return ResponseEntity.ok(new AuthResponseDto(userDetails.getUsername(), accessToken));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new AuthResponseDto(null, null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new AuthResponseDto(null, null));
         }
     }
 }
