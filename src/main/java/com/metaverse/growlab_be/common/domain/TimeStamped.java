061a0beaa45e24011147a717011d92250fa00b52
JDoyoung <airwalk6346@gmail.com>
2026-03-15 11:01:51 +0900

    feat: User 엔터티 설계와 역할(Role) Enum 클래스 생성

---
+++ src/main/java/com/metaverse/growlab_be/common/domain/TimeStamped.java
@@ -0,0 +1,24 @@
+package com.metaverse.growlab_be.common.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeStamped {
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+}
