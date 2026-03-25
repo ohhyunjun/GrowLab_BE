df0a663688a0f774f0fb71064fd4f248c002cdfb
donghyeonShin <dmgbe6437@g.hongik.ac.kr>
2026-03-25 19:20:09 +0900

    feat: plant 관련 기능에 germinatedAt 필드 도입

--- src/main/java/com/metaverse/growlab_be/plant/dto/PlantRequestDto.java
+++ src/main/java/com/metaverse/growlab_be/plant/dto/PlantRequestDto.java
@@ -1,12 +1,15 @@
 package com.metaverse.growlab_be.plant.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
 @Getter
 public class PlantRequestDto {
     private String name;           // 식물 이름
     private String plantStage;     // 현재 성장 단계
     private LocalDateTime plantedAt; // 심은 날짜
+    private LocalDateTime germinatedAt; // 발아 날짜
 }
