38709836df3e6e557038bc605663876ee903e983
donghyeonShin <dmgbe6437@g.hongik.ac.kr>
2026-03-29 15:22:43 +0900

    feat: notice 프로젝트 기초 구조 생성/핵심 엔티티 설계

---
+++ src/main/java/com/metaverse/growlab_be/notice/domain/Notice.java
@@ -0,0 +1,38 @@
+package com.metaverse.growlab_be.notice.domain;
+
+import com.metaverse.growlab_be.common.domain.TimeStamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "notice")
+public class Notice extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "device_serial")
+    private String deviceSerial;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(name = "is_read")
+    private boolean isRead = false;
+
+    @Column(name = "notice_type")
+    private String noticeType;
+
+    @Column(nullable = false)
+    private Integer priority;
+
+    @Column(name = "additional_date")
+    private LocalDateTime additionalDate;
+}
