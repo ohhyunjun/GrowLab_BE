5f3c47284ad8cb8e0ef2fe8e5ac94f7462dbc1de
JDoyoung <airwalk6346@gmail.com>
2026-04-05 12:21:43 +0900

    fix: @Repository 어노테이션 누락 수정

--- src/main/java/com/metaverse/growlab_be/auth/repository/UserRepository.java
+++ src/main/java/com/metaverse/growlab_be/auth/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.metaverse.growlab_be.auth.repository;
 
 import com.metaverse.growlab_be.auth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // 사용자계정(Username == AccountID)으로 User객체를 조회하는 메서드
     // 스프링시큐리티의 UserDetailsService가 사용함
